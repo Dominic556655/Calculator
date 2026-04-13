ebd7e8aadcb66f4e95b3e6eac1733160297eb750
Dominic <ichadominic5@gmail.com>
2026-04-13 20:53:46 +0100

    ensure vat template exists

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-t@=rfj7!7s95x3vc4a+yi*02ldxohg@%%e4pe3iy0de96zg8px')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,calculator.onrender.com").split(",")
+ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,calculator-0vok.onrender.com"
+).split(",")
 
 
 
